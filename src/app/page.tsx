49b0fc6d59d133c7a76e383a47968f220fd52115
Nenene.com <116019900+MunaChitambo22@users.noreply.github.com>
2024-10-20 16:34:35 +0200

    Footer Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import AboutUs from "@/components/AboutUs";
+import Faq from "@/components/Faq";
 import FeaturedProperties from "@/components/FeaturedProperties";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import HowItWorks from "@/components/HowItWorks";
 import Navbar from "@/components/Navbar";
 import Services from "@/components/Services";
+import Testimonials from "@/components/Testimonials";
+
 
 const Home = () => {
   return (<>
@@ -13,6 +17,9 @@ const Home = () => {
       <FeaturedProperties />
       <Services />
       <AboutUs />
+      <Testimonials />
+      <Faq />
+      <Footer />
   </>  
   );
 };
--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -1,6 +1,34 @@
+import { ABOUT_US_TEXT } from "@/constants"
+
 const AboutUs = () => {
   return (
-    <div></div>
+    <div className="container mx-auto my-20 border-b pb-10">
+      <div className="mb-8">
+        <h2 className="mb-6 text-center text-3xl tracking-tighter sm:text-4xl
+        lg:text-5xl">
+          About Us
+        </h2>
+        <p className="py-6 text-center text-lg tracking-tighter lg:pr-20">
+          {ABOUT_US_TEXT}
+        </p>
+      </div>
+      <div className="flex flex-wrap text-center">
+        <div className="w-full border-neutral-700 p-6 lg:w-1/2 lg:border-r">
+        <p className="bg-gradient-to-r from-blue-400 via-green-200 to-blue-500
+        bg-clip-text text-transparent lg:text-7xl">
+          5000+
+        </p>
+        <p className="my-8 font-medium">Partner since 2016</p>
+        </div>
+        <div className="w-full p-6 lg:w-1/2">
+        <p className="bg-gradient-to-r from-orange-400 via-red-200 to-orange-500
+        bg-clip-text text-transparent lg:text-7xl">
+          1500+
+        </p>
+        <p className="my-8 font-medium">Investments closed</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
